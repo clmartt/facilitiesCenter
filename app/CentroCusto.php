03cfc0853e66dbd3f36012bccc76a564706e5ca9
clmartt <clmartt@gmail.com>
2021-03-19 15:14:42 -0300

    ATUALIZAÇÕES

---
+++ app/CentroCusto.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CentroCusto extends Model
+{
+    //
+    protected $table = 'centro_custo';
+}
--- app/Usuario.php
+++ app/Usuario.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     protected $table = 'usuario';
+
+    function grupo(){
+
+        return $this->belongsTo('App\Grupo','id_grupo','id_grupo');
+    }
 }
